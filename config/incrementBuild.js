31fa959eea21b5dd2dbffca95f4edadb6b86f82e
peterbenoit <peterbenoit@gmail.com>
2025-04-04 17:30:18 -0400

    feat: Increment build number and update version in manifest and package.json

--- config/incrementBuild.js
+++ config/incrementBuild.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-// Path to the build info file
+// Path to all files we need to update
 const buildInfoPath = path.resolve(__dirname, '../buildInfo.json');
 const manifestPath = path.resolve(__dirname, '../src/manifest.json');
+const packagePath = path.resolve(__dirname, '../package.json');
 
-// Read the current build info
+// Read the current files
 const buildInfo = require(buildInfoPath);
 const manifest = require(manifestPath);
+const packageJson = require(packagePath);
 
 // Increment the build number
 buildInfo.buildNumber += 1;
@@ -15,7 +17,11 @@ buildInfo.buildNumber += 1;
 // Update version in manifest with build number
 // This preserves the semver format but adds build number
 const versionBase = manifest.version.split('.').slice(0, 3).join('.');
-manifest.version = `${versionBase}.${buildInfo.buildNumber}`;
+const newVersion = `${versionBase}.${buildInfo.buildNumber}`;
+
+// Update versions in both files
+manifest.version = newVersion;
+packageJson.version = newVersion;
 
 // Write the updated build info back to the file
 fs.writeFileSync(
@@ -31,8 +37,15 @@ fs.writeFileSync(
 	'utf8'
 );
 
+// Write the updated package.json back to the file
+fs.writeFileSync(
+	packagePath,
+	JSON.stringify(packageJson, null, 2),
+	'utf8'
+);
+
 console.log(`Build number incremented to ${buildInfo.buildNumber}`);
-console.log(`Version updated to ${manifest.version}`);
+console.log(`Version updated to ${newVersion} in both manifest.json and package.json`);
 
 // Return the build number so it can be used by webpack
 module.exports = buildInfo.buildNumber;
